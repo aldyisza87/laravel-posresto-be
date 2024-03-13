82232362dd7847ddc7fb022ce2fe0f846ee38325
Aldy Isza <aldyisza87@gmail.com>
2024-03-13 18:39:32 +0700

    add create edit

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,18 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
     // index
     public function index(Request $request)
     {
-        $users = DB::table('users')->paginate(10);
+        $users = DB::table('users')
+            ->when($request->input('name'), function ($query, $name) {
+                $query->where('name', 'like', '%' . $name . '%')->orWhere('email', 'like', '%' . $name . '%');
+            })
+
+            ->paginate(10);
         return view('pages.users.index', compact('users'));
     }
-
 
     // Create
     public function create()
@@ -24,12 +30,13 @@ class UserController extends Controller
     public function store(Request $request)
     {
         $request->validate([
-
             'name' => 'required',
             'email' => 'required|email|unique:users',
             'password' => 'required|min:8',
             'role' => 'required|in:admin,staff,user',
         ]);
+
+
     }
 
     public function show($id)
@@ -39,21 +46,42 @@ class UserController extends Controller
 
     public function edit($id)
     {
-        return view('pages.users.edit');
+        $user = User::findOrFail($id);
+        return view('pages.users.edit', compact('user'));
     }
 
     public function update(Request $request, $id)
     {
         $request->validate([
             'name' => 'required',
-            'email' => 'required|email|unique:users' . $id,
+            'email' => 'required',
             'role' => 'required|in:admin,staff,user',
         ]);
+
+        // update the request
+        $user = User::find($id);
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->role = $request->role;
+        $user->save();
+
+        //if password is not empty
+        if ($request->password) {
+            $user->password = Hash::make($request->password);
+            $user->save();
+        }
+
+        return redirect()
+            ->route('users.index')
+            ->with('success', 'User updated successfully');
     }
 
-    public function destroy($id){
+    public function destroy($id)
+    {
 
+        $user = User::find($id);
+        $user->delete();
+
+        return redirect()->route('users.index')->with('success', 'User deleted successfully');
     }
-
-
 }
